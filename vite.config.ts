9f066220d640b9ffe98a172bbcc298904aeeda7f
Aldo Martinez <acm.holmes@gmail.com>
2023-05-07 10:41:35 -0500

    adding github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), crossOriginIsolation()],
 });
